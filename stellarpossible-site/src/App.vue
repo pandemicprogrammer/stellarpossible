1b4dfe3b7a453e3715314ba30f6a252c3bca3846
Marie Valentonis <mlvalentonis@protonmail.com>
2024-04-08 17:29:35 -0400

    vue-router config and initial setup

--- stellarpossible-site/src/App.vue
+++ stellarpossible-site/src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
   <header>
     <div class="wrapper">
+      <!-- Wrap "StellarPossible" in a RouterLink to route back to home ("/") -->
+      <div class="logo">
+        <RouterLink to="/">StellarPossible</RouterLink>
+      </div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/Products">
+          Products
+        </RouterLink>
       </nav>
     </div>
   </header>
@@ -16,4 +22,13 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
+.wrapper {
+    display: flex;
+    justify-content: space-between;
+}
+/* Additional styling for the RouterLink to make it look consistent with your design */
+.logo a {
+    text-decoration: none;
+    color: inherit; /* This ensures the link color matches your logo color */
+}
 </style>
--- stellarpossible-site/src/views/HomeView.vue
+++ stellarpossible-site/src/views/HomeView.vue
@@ -1,12 +1,32 @@
 <template>
   <main>
-    <!-- <TheWelcome /> -->
-    <!-- Using the imported image -->
-    <img :src="HeroImage" alt="Galaxy Hero" />
+    <div class="hero-image">
+      <img :src="HeroImage" alt="Galaxy Hero" />
+    </div>
   </main>
 </template>
 
 <script setup>
-// import TheWelcome from '../components/TheWelcome.vue'
 import HeroImage from '../assets/images/galaxy-hero.png'
 </script>
+
+<style scoped>
+.hero-image {
+  position: relative;
+  width: 100%;
+}
+
+.hero-image img {
+  width: 100%;
+}
+
+.hero-image::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+background: radial-gradient(circle at center, rgba(13, 1, 11, 0) 0%, rgba(13, 1, 11, 1) 100%);
+}
+</style>
