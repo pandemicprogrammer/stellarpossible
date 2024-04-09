4bba3348b9b9d32b4ee9667a66ddfc37391ece1b
Marie Valentonis <mlvalentonis@protonmail.com>
2024-04-09 09:18:33 -0400

    add hamburger media querie nav switching, tagline HTML and styles

--- stellarpossible-site/src/views/HomeView.vue
+++ stellarpossible-site/src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <template>
   <main>
-    <div class="hero-image">
-      <img :src="HeroImage" alt="Galaxy Hero" />
+    <div class="wrapper">
+      <div class="hero-image">
+        <img :src="HeroImage" alt="Galaxy Hero" />
+      </div>
+      <div class="tagline-wrapper">    
+        <div class="tagline-text">
+          <span>To Inspire.</span>
+          <span>To Be Inspired.</span>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -11,6 +19,38 @@ import HeroImage from '../assets/images/galaxy-hero.png';
 </script>
 
 <style scoped>
+.wrapper {
+  position: relative;
+}
+.tagline-wrapper {
+  position: absolute;
+  height: 100vh;
+  width: 100%;
+  top: 0;
+}
+.tagline-text {
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 87%;
+}
+.hero-image {
+  position: relative;
+  height: 90vh; /* Set the height of the hero image to fill the viewport */
+  overflow: hidden;
+  display: flex;
+  justify-content: center; /* This centers the image horizontally */
+}
+
+.hero-image img {
+  border-radius: 8px;
+  height: 100%;
+  width: auto; /* Width is auto to maintain aspect ratio */
+  object-fit: cover;
+  object-position: center;
+}
+
 .hero-image {
   position: relative;
   height: 90vh; /* Set the height of the hero image to fill the viewport */
@@ -25,6 +65,7 @@ import HeroImage from '../assets/images/galaxy-hero.png';
   width: auto; /* Adjust width automatically to maintain aspect ratio */
   object-fit: cover; /* Cover the area, may crop the image if aspect ratios differ */
   object-position: center; /* Center the image within the area */
+  margin-left: -2.5rem;
 }
 
 .hero-image::after {
@@ -34,6 +75,6 @@ import HeroImage from '../assets/images/galaxy-hero.png';
   left: 0;
   width: 100%;
   height: 100%;
-  background: radial-gradient(circle at center, rgba(255, 255, 255, 0) 25%, #110312 100%);
+  background: radial-gradient(circle at center, rgba(255, 255, 255, 0) 75%, #110312 100%);
 }
 </style>
